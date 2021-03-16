cb9e1c918f6a76c7afca0dccd014d427c2ffbbde
Johnson Dubula <jdubula@iqbusiness.net>
2021-03-16 18:00:00 +0300

    modified like

--- WebApplication4/Models/ApplicationDBContext.cs
+++ WebApplication4/Models/ApplicationDBContext.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApplication4.Models
 {
-    public class ApplicationDBContext
+    public class ApplicationDBContext : IdentityDbContext
     {
     }
 }
