e56454afb377a66b13cba4095388928560ff5916
Johnson Dubula <jdubula@iqbusiness.net>
2021-03-16 18:00:00 +0300

    Aplication DB context

--- WebApplication4/Startup.cs
+++ WebApplication4/Startup.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebApplication4.Models;
 
 namespace WebApplication4
 {
@@ -25,6 +23,11 @@ namespace WebApplication4
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+           options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers();
+
             services.AddControllers();
         }
 
