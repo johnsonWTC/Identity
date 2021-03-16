050c2d89ac259be23b5f609faca64fe1c26d8d7f
Johnson Dubula <jdubula@iqbusiness.net>
2021-03-16 18:00:00 +0300

    modified like

--- WebApplication4/Models/Employee.cs
+++ WebApplication4/Models/Employee.cs
@@ -9,6 +9,7 @@ namespace WebApplication4.Models
     {
         public int EmployeeId { get; set; }
         public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
     }
