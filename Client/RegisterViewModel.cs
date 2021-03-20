ea68911ef924e44c0ed9c4f483a61cd7ef8fe6db
Johnson Dubula <jdubula@iqbusiness.net>
2021-03-20 18:00:00 +0300

    Auth

--- Client/RegisterViewModel.cs
+++ Client/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace Client
 {
     class RegisterViewModel
     {
-
+        [Required]
         public string Email { get; set; }
         public string Password { get; set; }
         public string ConfirmPassWord { get; set; }
