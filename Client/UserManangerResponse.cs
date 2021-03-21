8eebaadc35739b1d9cb8ce87aef8e0c1c2bf4b98
Johnson Dubula <jdubula@iqbusiness.net>
2021-03-21 18:00:00 +0300

    Auth

--- Client/UserManangerResponse.cs
+++ Client/UserManangerResponse.cs
@@ -10,5 +10,6 @@ namespace Client
     {
         public string Message  { get; set; }
         public bool isSuccess { get; set; }
+        public IEnumerable<string> Errors { get; set; }
     }
 }
