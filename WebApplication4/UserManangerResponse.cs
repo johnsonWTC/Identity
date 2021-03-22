72a95497d57ebd7664dabd74b0206488f73064c1
Johnson Dubula <jdubula@iqbusiness.net>
2021-03-22 18:00:00 +0300

    Auth

---
+++ WebApplication4/UserManangerResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedClass
+{
+    class UserManangerResponse
+    {
+        public string Message  { get; set; }
+        public bool isSuccess { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
