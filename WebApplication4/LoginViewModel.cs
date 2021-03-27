006e9c2b06807417bdcd2575c5da768ac8be4a39
Johnson Dubula <jdubula@iqbusiness.net>
2021-03-27 18:00:00 +0300

    Auth

--- WebApplication4/LoginViewModel.cs
+++ WebApplication4/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace WebApplication4
 {
     public class LoginViewModel
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
