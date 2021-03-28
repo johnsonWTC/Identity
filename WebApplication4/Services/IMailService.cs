25b1c5d8a258593383b4bfd74759b451d3fabaaa
Johnson Dubula <jdubula@iqbusiness.net>
2021-03-28 18:00:00 +0300

    Auth

---
+++ WebApplication4/Services/IMailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Services
+{
+    interface IMailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string content);
+    }
+
+    public class
+}
