7b0a9a757fc5d5040f8e111a011776ca606d6d09
Johnson Dubula <jdubula@iqbusiness.net>
2021-03-29 18:00:00 +0300

    Auth

--- WebApplication4/Services/IMailService.cs
+++ WebApplication4/Services/IMailService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +13,26 @@ namespace WebApplication4.Services
 
     public class SendGridMailService : IMailService
     {
+        private IConfiguration _configaration;
 
+        public SendGridMailService(IConfiguration configaration)
+        {
+            _configaration = configaration;
+        }
+
+
+
+        public Task SendEmailAsync(string toEmail, string subject, string content)
+        {
+            var apiKey = _configaration["SendGripkey"];
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("test@example.com", "Example User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("test@example.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
     }
 }
